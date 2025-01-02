6fc0d161fa5868d68ba54c22491ea2cd367ef1e5
山田幸助 <offmailbox1225@gmail.com>
2025-01-02 20:40:35 +0900

    Finish feature/local_db_insert(ReactでTodoアプリを作ろう_6完了)

--- vite-project/src/components/todos/index.tsx
+++ vite-project/src/components/todos/index.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import localforage from 'localforage';
+import { useNavigate } from 'react-router-dom';
 
+/***** "Todo"型の定義 *****/
 type Todo = {
   title: string;
   readonly id: number;
@@ -8,19 +11,35 @@ type Todo = {
 };
 
 type Filter = 'all' | 'completed' | 'unchecked' | 'delete';
- 
+
+/***** "Todo"コンポーネントの定義 *****/
 const Todo: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [text, setText] = useState<string>('');
   const [nextId, setNextId] = useState<number>(1);
   const [filter, setFilter] = useState<Filter>('all');
+  const navigate = useNavigate();
 
   const isFormDisabled = filter === 'completed' || filter === 'delete';
 
+
+  useEffect(() => {
+    localforage.getItem('todo-20240622').then((values) => {
+      if(values){
+        setTodos(values as Todo[]);
+      }
+    });
+  }, []);
+  useEffect(() => {
+    localforage.setItem('todo-20240622', todos);
+  }, [todos]);
+
+  // フィルタリング変更時のステート更新処理
   const handleFilterChange = (filter: Filter) => {
     setFilter(filter);
   };
 
+  // タスク追加ボタン押下時の処理
   const handleSubmit = () => {
     // 入力チェック
     if(!text) return;   // フォーム未入力時は何もしない
@@ -39,47 +58,30 @@ const Todo: React.FC = () => {
     setText('');
   };
 
-  const handleEdit = (id: number, value: string) => {
+  // タスク更新時のステート更新処理
+  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+    id: number,
+    key: K,
+    value: V
+  ) => {
     setTodos((todos) => {
       const newTodos = todos.map((todo) => {
         if (todo.id === id){
-          return {...todo, title: value};
+          return {...todo, [key]: value};
+        }else{
+          return todo;
         }
-        return todo;
-      });
-      // todos stateの更新
-      return newTodos; 
-    });
-  };
-
-  const handleCheck = (id: number, completed_flg: boolean) => {
-    setTodos((todos) => {
-      const newTodos = todos.map((todo) => {
-        if(todo.id === id){
-          return {...todo, completed_flg };
-        }
-        return todo;
       });
       return newTodos;
     });
   };
 
-  const handleRemove = (id: number, delete_flg: boolean) => {
-    setTodos((todos) => {
-      const newTodos = todos.map((todo) => {
-        if(todo.id === id){
-          return {...todo, delete_flg};
-        }
-        return todo;
-      });
-      return newTodos;
-    });
-  };
-
+  // タスク物理削除時の処理
   const handleEmpty = () => {
     setTodos((todos) => todos.filter((todo) => !todo.delete_flg));
   }
 
+  // フィルタリングされたタスクリストを取得
   const getFilteredTodos = () => {
     switch(filter){
       case 'completed':
@@ -95,6 +97,13 @@ const Todo: React.FC = () => {
 
   return (
     <div className="todo-container">
+      <button
+        className="back-button"
+        onClick={() => navigate('/')}
+        title="Topページに戻る"
+      >
+        ⇦ 戻る
+      </button>
       <select
        defaultValue="all"
        onChange={(e) => handleFilterChange(e.target.value as Filter)}
@@ -132,19 +141,19 @@ const Todo: React.FC = () => {
             <input
               type="checkbox"
               checked={todo.completed_flg}
-              onChange={() => handleCheck(todo.id, !todo.completed_flg)}
+              onChange={() => handleTodo(todo.id, 'completed_flg', !todo.completed_flg)}
             />
             <input
               type="text"
               value={todo.title}
               disabled={isFormDisabled}
-              onChange={(e) => handleEdit(todo.id, e.target.value)}
+              onChange={(e) => handleTodo(todo.id, 'title', e.target.value)}
             />
-            <button onClick={() => handleRemove(todo.id, !todo.delete_flg)}>
+            <button onClick={() => handleTodo(todo.id, 'delete_flg', !todo.delete_flg)}>
               {todo.delete_flg ? '復元' : '削除'}
             </button>
           </li>
-        ))}        
+        ))}
       </ul>
     </div>
   );
